3a0f998557e7e1ba5b01937893c6d83a377eaa32
che <che@ches-MacBook-Air.local>
2026-09-21 18:36:34 -0600

    arreglando fotos en render

--- config/settings.py
+++ config/settings.py
@@ -154,7 +154,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = Path(os.getenv('MEDIA_ROOT', BASE_DIR / 'media'))
 
 # HTTPS detrás del proxy de Render
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.static import serve
 from django.views.generic import RedirectView
 
 urlpatterns = [
@@ -28,5 +29,10 @@ urlpatterns = [
     path('notificaciones/', include('notificaciones.urls')),
     path('reportes/', include('reportes.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(
+        r'^media/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.MEDIA_ROOT},
+    ),
+]
